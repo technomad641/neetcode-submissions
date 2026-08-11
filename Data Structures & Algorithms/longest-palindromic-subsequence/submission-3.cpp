ad1a00bbf1300bf507c6af7e158db5793b5ceab6
Rajat Singh <singhrajat641@gmail.com>
2026-08-10 22:38:45 -0700

    Add: longest-palindromic-subsequence - submission-3

---
+++ Data Structures & Algorithms/longest-palindromic-subsequence/submission-3.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    unordered_map< int, unordered_map< int, int > > dp;
+    int recur(int i, int j, string s){
+        if(i>j) return 0;
+        if(i==j) return dp[i][j]=1;
+        if(s[i]==s[j]) return dp[i][j]=2+recur(i+1, j-1, s);
+        if(dp.find(i)!=dp.end() && dp[i].find(j)!= dp[i].end()){
+            return dp[i][j];
+        }
+        return dp[i][j] = max(recur(i+1, j, s), recur(i, j-1, s));
+    }
+    int longestPalindromeSubseq(string s) {
+        int sz = s.length();
+        int maxlen =0;
+        return recur(0, sz-1, s);
+    }
+};
