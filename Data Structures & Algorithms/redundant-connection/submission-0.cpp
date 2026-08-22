f2991acb760950d460b5021e91e74af7a9637299
Rajat Singh <singhrajat641@gmail.com>
2026-08-22 00:01:20 -0700

    Add: redundant-connection - submission-0

---
+++ Data Structures & Algorithms/redundant-connection/submission-0.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector< int > parent;
+    int find(int a){
+        if(a == parent[a]) return a;
+        return find(parent[a]);
+    }
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int sz=edges.size();
+        parent.resize(sz+1, 0);
+        vector<int>ans;
+        for(int i=1;i<=sz;i++) parent[i]=i;
+        for(auto edge: edges){
+            int a = edge[0];
+            int b = edge[1];
+            cout<< find(a)<<" "<< find(b)<<endl;
+            if(find(a)!= find(b)){
+                parent[find(a)] = find(b);
+            }else{
+                ans.push_back(a);
+                 ans.push_back(b);
+                 return ans;
+            }
+        }
+        return ans;
+    }
+};
